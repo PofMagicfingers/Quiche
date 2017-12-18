80b45dc30a1fb79e1169c16fdac8dae85cddf07c
Giovanni Olivera <giovanni@commit42.fr>
2017-12-18 19:28:11 +0100

    Ignore .build

--- .build/main/libraries/Arduboy2/ab_logo.c.d
+++
@@ -1,2 +0,0 @@
-/home/pof/Documents/Code/perso/noel/.build/main/libraries/Arduboy2/ab_logo.c.o: \
- /home/pof/Arduino/libraries/Arduboy2/src/ab_logo.c
--- .build/main/libraries/Arduboy2/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-/home/pof/Documents/Code/perso/noel/.build/main/libraries/Arduboy2/glcdfont.c.o: \
- /home/pof/Arduino/libraries/Arduboy2/src/glcdfont.c
