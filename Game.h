3d3e263f0fd9b950439449f6bd10a7cea0ff70d0
Pof Magicfingers <pof@pof.pm>
2017-12-29 16:12:54 +0100

    Cleaning

---
+++ Game.h
@@ -0,0 +1,67 @@
+#ifndef GAMESINGLETON_H
+#define GAMESINGLETON_H
+
+#include <Arduboy2.h>
+
+#include "src/stages/Stage.h"
+#include "src/stages/Hand/Hand.h"
+#include "src/stages/Beer/Beer.h"
+
+enum class GameStatus
+{
+    BOOTING = 0,
+    MENU,
+    PLAYING,
+    GAMEOVER,
+    SCOREBOARD
+};
+
+class Game
+{
+  private:
+    Game &operator=(const Game &) {}
+    Game(const Game &) {}
+    Game();
+    ~Game();
+
+  public:
+    static Game *SharedInstance()
+    {
+        if (NULL == _singleton)
+        {
+            _singleton = new Game();
+        }
+        return _singleton;
+    }
+    static void kill()
+    {
+        if (NULL != _singleton)
+        {
+            delete _singleton;
+            _singleton = NULL;
+        }
+    }
+
+    void setup();
+    void loop();
+
+  private:
+    void menu();
+    void gameover();
+    void playing();
+
+    static Game *_singleton;
+
+    Arduboy2 *arduboy;
+    Stage *currentStage;
+    BoomBox *boomBox;
+    int stageDone = 0;
+    GameStatus game_status = GameStatus::MENU;
+
+    int quichy = -50;
+    bool playedBootSound = false;
+    int showStartText = 0;
+};
+
+Game *Game::_singleton = NULL;
+#endif
